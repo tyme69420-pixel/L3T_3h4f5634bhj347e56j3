d872d46bf007bfd80e3c9365e8791ae540028197
tyme69420-pixel <tyme69420@gmail.com>
2026-03-14 13:41:27 +0700

    Retrieve and log computer hostname and IP address
    Added functionality to retrieve and display hostname and IP address.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import socket
 
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+print("I hacked into yocur pc i know your ip addr")
+print("it is\n")
+
+print("Your Computer Name is:", hostname)
+print("Your Computer IP Address is:", IPAddr)
+
+with open(log.txt, "w") as file:
+  file.write("%s | %s" %(hostname, IPAddr))
+  

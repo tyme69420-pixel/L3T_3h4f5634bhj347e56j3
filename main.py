a220aa374dc10253011afacd3e08ef5be923611f
Eletroman179 <jameschaowadee@gmail.com>
2026-03-18 11:01:33 +0700

    Add warning and input prompt for IP sending
    Added warning and input prompt before sending IP to Discord webhook.

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def ip():
     host = socket.gethostname()
     return socket.gethostbyname(host)
 
+print("WARNING THIS SENDS YOUR IP")
+input("press enter to start or ctrl+c to stop")
+
 requests.post("https://discord.com/api/webhooks/1483326994253873216/o21iquTxU4nb_YQzQVPh3iemcMLjj-NiqHQwCW_YUktTf3EUVhCnUiX2hRk4vLlaTZnp", {
     "content": json.dumps(
         {
